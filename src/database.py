372560fbcb8ce337c7825ce018431c4a027ce6d1
unknown <edisonbenjcao@gmail.com>
2026-05-15 12:49:55 -0500

    feat: add iteration to use ~25 years of data from HKMA

--- src/database.py
+++ src/database.py
@@ -5,7 +5,10 @@ import pandas as pd
 def data_to_df(bis_results, hkma_results):
     df_bis_hkcreditgap = pd.read_csv(io.StringIO(bis_results[0]))
     df_bis_hkpropindex = pd.read_csv(io.StringIO(bis_results[1]))
-    df_hkma_hkaggbalance = pd.DataFrame(hkma_results[0]["result"]["records"])
+    df_hkma_hkaggbalance = pd.concat(
+        [pd.DataFrame(hkma_results[i]["result"]["records"]) for i in range(9)],
+        ignore_index=True,
+    )
     df_hkma_hkaggbalance["end_of_date"] = pd.to_datetime(
         df_hkma_hkaggbalance["end_of_date"]
     )
